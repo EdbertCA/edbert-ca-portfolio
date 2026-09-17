bc0aae145210fa609c257ed2d956c20c1b5418d9
Edbert C Angwyn <edbert.ca05@gmail.com>
2026-09-17 21:18:31 +0700

    Adding stubs for pages components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Skills from "./pages/Skills"
+import Contact from "./pages/Contact"
+
 export default function App() {
   return (
-    <main>
-      <h1>Edbert Angwyn</h1>
-      <p>Software Developer</p>
-    </main>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  )
 }
