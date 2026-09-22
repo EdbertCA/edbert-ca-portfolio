112b8e9399cc3c2732d483148fd96c4a543350ac
Edbert C Angwyn <edbert.ca05@gmail.com>
2026-09-22 10:34:27 +0700

    feat(Footer): Include styled footer component to every page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from "./pages/About"
 import Projects from "./pages/Projects"
 import Contact from "./pages/Contact"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Mail } from 'lucide-react'
+
+export default function Footer() {
+  return (
+    <footer className="mt-3 border-t border-slate-200">
+      <div className="mx-auto flex gap-3 flex-col sm:flex-row max-w-7xl items-center justify-between px-8 py-6">
+        <p>Build by Edbert C Angwyn · React + Tailwind</p>
+        <div className="flex gap-5 items-center">
+          <a
+            href="https://github.com/EdbertCA"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-slate-500 hover:text-blue-700"
+          >
+            <FaGithub className="h-6 w-6"/>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/edbert-clarence-angwyn/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-slate-500 hover:text-blue-700"
+          >
+            <FaLinkedin className="h-6 w-6"/>
+          </a>
+          <a 
+            href="mailto:edbert.ca05@gmail.com"
+            className="text-slate-500 hover:text-blue-700"
+          >
+            <Mail className="h-6 w-6"/>
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
