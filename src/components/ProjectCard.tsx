946ab7b27d82303cc9e9a0ebde7fe6cde5c786ea
Edbert C Angwyn <edbert.ca05@gmail.com>
2026-09-24 22:41:30 +0700

    feat(ProjectCard): Adding hovering effect animation on ProjectCard component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,7 +11,7 @@ interface ProjectCardProps {
 export default function ProjectCard({title, description, technologies, image, github, demo, index}: ProjectCardProps) {
   const swapped = index % 2 === 0 ? "md:flex-row-reverse" : "md:flex-row";
   return (
-    <article className={`flex flex-col max-w-7xl items-center gap-10 px-5 py-8 bg-gray-100 mb-7 rounded-2xl  ${swapped}`}>
+    <article className={`flex flex-col max-w-7xl items-center gap-10 px-5 py-8 bg-gray-100 mb-7 rounded-2xl transition-transform duration-250 hover:-translate-y-2 hover:shadow-lg ${swapped}`}>
       <section className="md:w-1/2">
         <h2 className="text-3xl font-semibold">{title}</h2>
         <p className="my-2  text-slate-600">{description}</p>
